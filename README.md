0950c8b50303e9684f74dbed6734452f0bc82f88
itspinky95 <itspinky95@hotmail.com>
2022-10-23 21:30:41 +0100

    building js script and finishing readme.md file

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
     Building a HTML, CSS and JavaScript app to generate random quotes on screen.
 
 ## Testing Webpage
-    I personally recommend using VS Code Extensions 'Live Server'. or if you are not using file explore just open 'index.html'. if you have access to the internet i will have it live in my projects on my portfolio at scottwjones.co.uk
+    I personally recommend using VS Code Extensions 'Live Server'. or if you are not using file explore just open 'index.html'. if you have access to the internet i will have it live in my projects on my portfolio at https://scottwjones.co.uk
 
 ## Build 
 1.  Build Readme.md file.
@@ -10,7 +10,11 @@
 3.  Testing basic HTML and css on live server. worked fine. 
 4.  Making arrey for the Quotes in script.js
 5.  Making getRandomQuote function getting a random quote from the arrey 
-6.  Making printQuote function ........
+6.  Making a list of colours to change though
+7.  Making printQuote function printing a quote to the html page
+8.  Making a function to change the colour on the page 
+9.  Setting intervals for changing quote and colour
+10. an event listener for changing quote ad colour on demand
 
 ## Contributing
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+/*** 
+ * `quotes` array of objects
+***/
+
 const quotes = [
   // quote number zero
   {
@@ -75,35 +84,49 @@ const quotes = [
 /***
  * `getRandomQuote` function
 ***/
-function getRandomQuote () {
-  let randomQuote = quotes[Math.floor(Math.random () * quotes.length)]; 
+function getRandomQuote() {
+  let randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
   return randomQuote;
 };
+
+
+
+/*
+ * setting colour of background
+*/
+const colors = ['red', 'blue', 'violet', 'hotpink', 'brown', 'purple', 'green', 'orange', 'gray', 'babyblue'];
+
 
 /***
  * `printQuote` function
 ***/
 
 
-function printQuote () {
-  let quote = getRandomQuote ();
+function printQuote() {
+  document.body.style.backgroundColor = getRandomColor();
+  let quote = getRandomQuote();
   let html = `<p class="quote">${quote.quote}</p>
    <p class="source">${quote.source}`
-    if (quote.citation !== undefined) {
-     html += `<span class ="citation">${quote.citation}</span>`;
-    }
-    if (quote.year !== undefined) {
-     html += `<span class = "year">${quote.year}</span>`;
-    }
-    if (quote.tag !== undefined) {
-      html += `<span class = "tag">, ${quote.tag}</span>`;
-     }
-    `</p>`
-  
-      return(document.getElementById("quote-box").innerHTML = html);
+  if (quote.citation !== undefined) {
+    html += `<span class ="citation">${quote.citation}</span>`;
   }
-   // interval to rotate between color and quotes, set for five seconds
-  setInterval(printQuote, 4000);
+  if (quote.year !== undefined) {
+    html += `<span class = "year">${quote.year}</span>`;
+  }
+  if (quote.tag !== undefined) {
+    html += `<span class = "tag">, ${quote.tag}</span>`;
+  }
+  `</p>`
+
+  return (document.getElementById("quote-box").innerHTML = html);
+}
+
+// generates a random color from the colors array on button click
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * 9)]
+}
+// interval to rotate between color and quotes, set for five seconds
+setInterval(printQuote, 15000);
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
